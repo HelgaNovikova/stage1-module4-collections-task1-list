c1a3f2255297726c1d9ea53a3701cd4fb70fa827
HelgaNovikova <helpotaki@rambler.ru>
2022-11-15 14:00:39 -0500

    lists st1m4

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> sortedList = new LinkedList<>();
+        for (Integer item:sourceList){
+            if (item%2==0){
+                sortedList.addLast(item);
+            }
+            else sortedList.addFirst(item);
+        }
+        return sortedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int intA = Integer.parseInt(a);
+        int intB = Integer.parseInt(b);
+        if (Math.abs(intA)==Math.abs(intB)){
+            return intA - intB;
+        }
+        return (int) ((5 * Math.pow(intA,2) +3) -  (5 * Math.pow(intB,2) +3));
     }
 }
